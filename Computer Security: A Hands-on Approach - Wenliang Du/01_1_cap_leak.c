27d58d7ab92be4de7bc3b9aa528691f1cd9fa382
Josh Birch <joshuabirch@protonmail.com>
2026-02-27 16:07:10 +0000

    Read up to chapter 1.4.4 in "Computer Security: Hands on Approach"

---
+++ Computer Security: A Hands-on Approach - Wenliang Du/01_1_cap_leak.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void main()
+{
+  int fd;
+  char *v[2];
+
+  // Assume that /etc/zzz is an important system file
+  fd = open("/etc/zzz", O_RDWR | O_APPEND);
+
+  if (fd == -1)
+  {
+    printf("Cannot open /etc/zzz\n");
+    exit(0);
+  }
+
+  // Print out the file descriptor value
+  printf("fd is %d\n", fd);
+
+  // Permanently disable the privilege by making the effective uid
+  // the same as the real uid
+  setuid(getuid());
+
+  // Execute /bin/sh
+  v[0] = "/bin/sh";
+  v[1] = 0;
+  execve(v[0], v, 0);
+}
